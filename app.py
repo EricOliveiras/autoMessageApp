302472357cd86a7902535260a3cd43eeccee1b71
Eric Oliveira <oiericoi@hotmail.com>
2024-08-13 21:49:43 -0300

    refactor: modify app file

--- app.py
+++ app.py
@@ -1,14 +1,27 @@
 import os
+import threading
+import time
+import webbrowser
 
 from flask import Flask
 
 from database.db import create_database
+from route import create_routes
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates', static_folder='static')
+
+create_routes(app)
+
+
+def open_browser():
+    time.sleep(5)
+    webbrowser.open('http://127.0.0.1:5000')
+
 
 if __name__ == '__main__':
     if not os.path.exists('contatos.db'):
         create_database()
     else:
         print("O banco de dados já existe.")
+    threading.Thread(target=open_browser).start()
     app.run()
